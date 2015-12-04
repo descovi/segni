9630929bd62f7f69ce40e3a52c5c46b2fe229b65
Alessandro <descovi@gmail.com>
2015-12-04 12:42:24 +0100

    added convert options

--- app/models/opera.rb
+++ app/models/opera.rb
@@ -1,8 +1,14 @@
 class Opera < ActiveRecord::Base
+  # d is for dynamic
+  # c is for cropped
   has_attached_file :image, styles: { 
-    medium: '300x300>', 
-    thumb:  '100x100#', 
-    normal: '1024x768>' 
+    _100x100c:  '100x100#',
+    _1024x768d: '1024x768>' 
+  }, 
+  convert_options:  { 
+    _100x100c: '-quality 75 -strip',
+    _1024x768d: '-quality 90 -strip'
   }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates :image, :website, presence: true
 end
