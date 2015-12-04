7a5f7afbcd76d15067442993fbde3c0a0f0ced4c
Alessandro <descovi@gmail.com>
2015-12-04 12:10:30 +0100

    Attached file image for opera.

--- app/models/opera.rb
+++ app/models/opera.rb
@@ -1,3 +1,8 @@
 class Opera < ActiveRecord::Base
-  
+  has_attached_file :image, styles: { 
+    medium: '300x300>', 
+    thumb:  '100x100#', 
+    normal: '1024x768>' 
+  }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
