ee8345330f6b4e84383ab713ee19012d5fd93d01
Alessandro <descovi@gmail.com>
2015-12-05 16:25:22 +0100

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204122253) do
+ActiveRecord::Schema.define(version: 20151204203129) do
 
   create_table "operas", force: :cascade do |t|
     t.text     "name"
@@ -33,14 +33,13 @@ ActiveRecord::Schema.define(version: 20151204122253) do
     t.datetime "updated_at", null: false
     t.string   "title"
     t.text     "content"
+    t.string   "slug"
   end
 
   add_index "page_translations", ["locale"], name: "index_page_translations_on_locale"
   add_index "page_translations", ["page_id"], name: "index_page_translations_on_page_id"
 
   create_table "pages", force: :cascade do |t|
-    t.text    "title"
-    t.text    "content"
     t.integer "website_id"
   end
 
