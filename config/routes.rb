a91ab61d1187c0c2fc97c24b28f27d67a8b7b817
Alessandro <descovi@gmail.com>
2015-12-05 16:28:51 +0100

    routing under locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  scope "/:locale" do
+    resources :operas
+    resources :websites
+    resources :pages
 
-  resources :operas
-  resources :websites
-  resources :pages
-  root 'websites#show'
+    root 'websites#show'
+  end
 end
