004af026a6f0256c3cde9e782a30b2fe627af1c3
Alessandro <descovi@gmail.com>
2015-12-05 16:21:50 +0100

    Set locale before action and default url options for internazionalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :get_site
+  before_action :get_site
+  before_action :set_locale
+  
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
 
-private
+  def set_locale
+    I18n.locale = params[:locale]
+  end
+  
   def get_site
     @website = Website.find_by(name_slug: request.subdomain)
   end
