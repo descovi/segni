31901669c36c61fe64df97b0233c88b998f67e54
Alessandro <descovi@gmail.com>
2015-12-07 15:27:43 +0100

    With translates :slug the url turn back full translated

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,7 +2,7 @@ class Page < ActiveRecord::Base
   belongs_to :website
   has_many :page_translations
   
-  translates :title, :content
+  translates :title, :content, :slug
   extend FriendlyId
   friendly_id :title, use: [:globalize]
 end
