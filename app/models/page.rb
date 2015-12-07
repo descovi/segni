2129a00d255ea2f2c96eee4a7ef958e6f01bc79d
Alessandro <descovi@gmail.com>
2015-12-07 15:28:22 +0100

    Globalize accessor for modify content of page by admin page.

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,5 +4,7 @@ class Page < ActiveRecord::Base
   
   translates :title, :content, :slug
   extend FriendlyId
-  friendly_id :title, use: [:globalize]
+  friendly_id :title, use: :globalize
+
+  globalize_accessors locales: I18n.available_locales, attributes: [:title, :content]
 end
