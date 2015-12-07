816edba11d4865032f4437283f491d19aba1f451
Alessandro <descovi@gmail.com>
2015-12-07 14:07:19 +0100

    User can be create / Menu for great admin

--- app/controllers/great_admin/websites_controller.rb
+++ app/controllers/great_admin/websites_controller.rb
@@ -1,5 +1,6 @@
 class GreatAdmin::WebsitesController < GreatAdminController
-  
+    before_action :set_website, only: [:show, :edit, :update, :destroy]
+
   def index
     @websites = Website.all
   end
@@ -18,29 +19,33 @@ class GreatAdmin::WebsitesController < GreatAdminController
   
   def create
     @website = Website.new(website_params)
+    if @website.save
+      redirect_to [:great_admin, Website], notice: 'website was successfully created.'
+    else
+      render :new
+    end
+  end
 
-    respond_to do |format|
-      if @website.save
-        format.html { redirect_to [:great_admin, Website], notice: 'website was successfully created.' }
-        format.json { render :show, status: :created, location: @website }
-      else
-        format.html { render :new }
-        format.json { render json: @website.errors, status: :unprocessable_entity }
-      end
+  def update
+    if @website.update(website_params)
+      redirect_to [:great_admin, @website]
+    else
+      render :edit
     end
   end
   
   def destroy
     @website = Website.find params[:id]
     @website.destroy
-    respond_to do |format|
-      format.html { redirect_to [:great_admin, Website], notice: 'Opera was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to [:great_admin, Website]
   end
 
   def website_params
     params.require(:website).permit(:name, :year, :archive_index, :website_id, :image)
+  end
+
+  def set_website
+    @website = Website.find params[:id]
   end
 
 end
--- app/views/great_admin/websites/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @website, :id, :name, :name-slug, :primary_color, :secondary_color, :created_at, :updated_at
