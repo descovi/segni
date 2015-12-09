c6117b767d189428705724df4a446a4aad2793cc
Alessandro <descovi@gmail.com>
2015-12-09 18:53:55 +0100

    big style for background

--- app/models/simple_block.rb
+++ app/models/simple_block.rb
@@ -1,5 +1,9 @@
 class SimpleBlock < ActiveRecord::Base
   belongs_to :page
-  has_attached_file :background_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :background_image, styles: { 
+    medium: "300x300>", 
+    thumb: "100x100>",
+    big: "1024x1024#"
+  }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :background_image, content_type: /\Aimage\/.*\Z/
 end
