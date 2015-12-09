63754dc950b7c324d03ea9108f67fbca67de02b8
Alessandro <descovi@gmail.com>
2015-12-09 22:00:07 +0100

    added big_hq as style

--- app/models/simple_block.rb
+++ app/models/simple_block.rb
@@ -3,7 +3,8 @@ class SimpleBlock < ActiveRecord::Base
   has_attached_file :background_image, styles: { 
     medium: "300x300>", 
     thumb: "100x100>",
-    big: "1024x1024#"
+    big: "1024x1024#",
+    big_hq: "1600x1200"
   }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :background_image, content_type: /\Aimage\/.*\Z/
 end
