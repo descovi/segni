2c1e26d5f66317abc49b34d34ca4c4835d43e13d
Alessandro <descovi@gmail.com>
2015-12-24 18:28:18 +0100

    Page showed only if you are in correct websites

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-  def show
-    @page = Page.friendly.find params[:id]
+  def show    
+    @page = Page.where(website: @website).friendly.find(params[:id])
     @title = @page.title
   end
 end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,23 @@
+class PagesControllerTest < ActionController::TestCase
+
+  setup do
+    website = websites(:website_one).name_slug
+    @request.host = "#{website}.test.host"
+  end
+  
+  test "should get page from slug" do
+    page = pages(:page_one)
+    
+    get :show, id: page.id, locale: 'it'
+
+    assert_response :success
+    assert_not_nil assigns(:page)
+  end
+
+  test "dont get page from slug (different website)" do
+    page = pages(:page_two)
+    assert_raises(ActiveRecord::RecordNotFound) do
+      get :show, id: page.id, locale: 'it'
+    end
+  end
+end
