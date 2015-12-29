0eff3e153201db8c7d7a15c9c87361b33150105b
Alessandro <descovi@gmail.com>
2015-12-29 19:30:38 +0100

    ApplicationHelper: path for create block with custom type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def path_for_create_block custom_type
-    [:admin, SimpleBlock.new, page_id: @page.id]
+    [:admin, SimpleBlock.new, page_id: @page.id ,custom_type: custom_type]
   end
 end
