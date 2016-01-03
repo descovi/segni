bc4ad14d2e16eec16cbd8ca002ebdd82d59548fb
Alessandro <descovi@gmail.com>
2016-01-04 00:41:51 +0100

    Products from other websites

--- app/models/website.rb
+++ app/models/website.rb
@@ -7,4 +7,7 @@ class Website < ActiveRecord::Base
   def products_first_page
     products.where(first_page: true)
   end
+  def products_from_other_websites
+    Product.where.not(website_id: self.id)
+  end
 end
