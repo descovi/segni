7d5c29ce7fb18c43fe92371604261b11bb3e86ad
Alessandro <descovi@gmail.com>
2016-01-28 16:52:39 +0100

    remove some comments

--- test/controllers/admin/pages_controller_test.rb
+++ test/controllers/admin/pages_controller_test.rb
@@ -5,17 +5,13 @@ class Admin::PagesControllerTest < ActionController::TestCase
 
   setup do
     @website = websites(:website_one)
-    
     @request.host = "#{@website.name_slug}.test.host"
   end
 
   test 'set first page' do
     sign_in users(:user_one)
     page = pages(:page_other)
-    #post #admin_page_first_page_path(page_id: pages(:page_one).id)
-    # post admin_page_first_page_path(page_id: page.id, locale: 'it')
     post :first_page, {locale: 'it', page_id: page.id}
-    # post "first_page", id: pages(:page_one).id, locale: 'it'
     assert_response :redirect
     @website.reload
     assert_equal @website.first_page, page
