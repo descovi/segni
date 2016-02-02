21f3d02af1ae19012e8fe285ab6b07c34f2a83fe
Alessandro <descovi@gmail.com>
2016-02-02 18:09:23 +0100

    menu as list

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -5,7 +5,8 @@ class Menu < ActiveRecord::Base
   validates :website, presence: true
   validates :position, presence: true
   validates :position, uniqueness: { scope: :website_id, message: "This is position is already used" }
-
+  acts_as_list scope: :website
+  
   def self.last_position
     return last.position if last.present?
     0
