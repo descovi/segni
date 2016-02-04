1306aea048ab53a6977c8c5712ed3112513fe373
Alessandro <descovi@gmail.com>
2016-02-04 10:54:35 +0100

    model for image

---
+++ app/models/image.rb
@@ -0,0 +1,7 @@
+class Image < ActiveRecord::Base
+  has_attached_file :url, 
+                      styles: { _1150x850d: "1150x850>" }, 
+                      convert_options: { _1150x850d: '-quality 90' }
+  crop_attached_file :url
+  validates_attachment :url, content_type: /\Aimage\/.*\Z/
+end
