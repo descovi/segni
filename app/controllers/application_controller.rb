dd075af66072130491f16f3f1a8574d5a96746e0
Alessandro <descovi@gmail.com>
2016-02-06 19:16:39 +0100

    base_url instead host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   end
   
   def get_site
-    @website = Website.find_by(domain: request.host)
+    @website = Website.find_by(domain: request.base_url)
     @website = Website.find_by(name_slug: request.subdomain) if @website.nil?
   end
   
