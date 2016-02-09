57f65110e711afc2740f889ea700ed61bc4ddffa
Alessandro <descovi@gmail.com>
2016-02-09 19:37:57 +0100

    new options for image table

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,14 @@
 class Image < ActiveRecord::Base
-  has_attached_file :url, styles: { _100x100c: "100x100", _1150x850d: "1150x850>" }, 
-                          convert_options: { _100x100c: "-quality 90",_1150x850d: '-quality 90' }
+  has_attached_file :url, styles: { 
+    _100x100c: "100x100", 
+    _1150x850d: "1150x850#",
+    block6: "390x390#"
+  }, 
+  convert_options: { 
+    _100x100c: "-quality 90",
+    _1150x850d: '-quality 90',
+    block6: '-quality 90 -strip'
+  }
   crop_attached_file :url
   validates_attachment_content_type :url, content_type: /\Aimage\/.*\Z/
   belongs_to :simple_block
