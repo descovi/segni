577d35f53707112932f061ebe354023cfdfb3b24
Alessandro <descovi@gmail.com>
2016-02-18 15:07:47 +0100

    Check if current user can edit this website

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,16 @@
 class AdminController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_if_user_is_owner
   layout 'admin-page'
+  
+  def check_if_user_is_owner
+    if current_user_can_edit_website? == false
+      return render text: 'You dont have permission for this website.'
+    end
+  end
+  
+  def current_user_can_edit_website?
+    return true if @website.user == current_user
+    false
+  end
 end
