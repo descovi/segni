07b18138ecbe7d68f50cf6520a00ae6541bd3d3b
Alessandro <descovi@gmail.com>
2016-04-14 11:31:57 +0200

    other quality

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,7 @@ class Image < ActiveRecord::Base
     _100x100c:  '100x100#', 
     _1150x850d: '1150x850#',
     _1150x450d: '1150x450#',
+    _1150x250d: '1150x250#',
     block6:     '390x390#',
     _300height: '1150x300#'
   }, 
@@ -10,6 +11,7 @@ class Image < ActiveRecord::Base
     _100x100c:  '-quality 90 -strip',
     _1150x850d: '-quality 90 -strip',
     _1150x450d: '-quality 90 -strip',
+    _1150x250d: '-quality 90 -strip',
     block6:     '-quality 90 -strip',
     _300height: '-quality 90 -strip'
   }
