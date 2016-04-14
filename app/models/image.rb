6ce0af8d9431e871a32428b51f5c92e8b7bc020d
Alessandro <descovi@gmail.com>
2016-04-14 09:46:09 +0200

    added quality with less height

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,12 +2,14 @@ class Image < ActiveRecord::Base
   has_attached_file :url, styles: { 
     _100x100c:  '100x100#', 
     _1150x850d: '1150x850#',
+    _1150x450d: '1150x450#',
     block6:     '390x390#',
     _300height: '1150x300#'
   }, 
   convert_options: { 
     _100x100c:  '-quality 90 -strip',
     _1150x850d: '-quality 90 -strip',
+    _1150x450d: '-quality 90 -strip',
     block6:     '-quality 90 -strip',
     _300height: '-quality 90 -strip'
   }
