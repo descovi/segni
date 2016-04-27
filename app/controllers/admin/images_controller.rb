1b0fc10fd5bad075c15b4cbc37d742a77b59070e
Alessandro <descovi@gmail.com>
2016-04-27 17:12:52 +0200

    image preview

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -7,9 +7,23 @@ class Admin::ImagesController < AdminController
   end
 
   def edit
-    @preview = @page
+    @preview = @image.simple_block.page
     render 'form', layout: 'admin-page'
   end
+
+  def update
+    @preview = @image
+    respond_to do |format|
+      if @image.update(images_params)
+        format.html { redirect_to [:edit, :admin, @image], notice: 'Image was successfully updated.' }
+        format.json { render :show, status: :ok, location: @image }
+      else
+        format.html { render :edit }
+        format.json { render json: @image.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def images_params
