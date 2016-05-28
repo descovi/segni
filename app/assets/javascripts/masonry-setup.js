c77c3e53fffe10b59367e6e3a1330cdd6427e0b5
Alessandro <descovi@gmail.com>
2016-05-28 14:05:11 +0200

    first prototype that is ok

---
+++ app/assets/javascripts/masonry-setup.js
@@ -0,0 +1,13 @@
+function masonry_setup(){
+  var $grid = $('.grid').imagesLoaded( function() {
+    $grid.masonry({
+      // set itemSelector so .grid-sizer is not used in layout
+      itemSelector: '.grid-item',
+      // use element for option
+      columnWidth: '.grid-sizer',
+      percentPosition: true,
+      gutter: '.gutter-sizer'
+    });
+  });
+
+}
