1b27c9485d1fd4a9d40ab554181c99cc0ded21bc
Alessandro <descovi@gmail.com>
2016-06-24 19:07:08 +0200

    filter for set resource

--- app/controllers/great_admin/surfaces_controller.rb
+++ app/controllers/great_admin/surfaces_controller.rb
@@ -1,15 +1,15 @@
 class GreatAdmin::SurfacesController < GreatAdminController
+
+  before_action :set_surface, only: [:show, :edit, :update, :destroy]
+
   def index
     @surfaces = Surface.all
   end
+
   def new
     @surface = Surface.new
   end
 
-  def edit
-    @surface = Surface.find params[:id]
-  end
-  
   def create
     @surface = Surface.new(surface_params)
 
@@ -31,7 +31,19 @@ class GreatAdmin::SurfacesController < GreatAdminController
     render :edit
   end
 
+
+  def destroy
+    @surface.destroy
+    redirect_to [:great_admin, Surface]
+  end
+
+
   def surface_params
     params.require(:surface).permit(:name, :name_it, :name_en)
   end
+
+  def set_surface
+    @surface = Surface.find params[:id]
+  end
+
 end
