9cb66936c7c74c1c9e8357618bac0e96148529f1
Alessandro <descovi@gmail.com>
2016-06-24 15:13:46 +0200

    test a page with a blocks of operas

--- test/controllers/operas_controller_test.rb
+++ test/controllers/operas_controller_test.rb
@@ -7,11 +7,12 @@ class OperasControllerTest < ActionController::TestCase
   
   test "should get opera from id" do
     opera = operas(:opera_one)
-    
-    get :show, id: opera.id, locale: 'it'
-
+   ['it', 'en'].each do |lng| 
+    get :show, id: opera.id, locale: lng
     assert_response :success
     assert_not_nil assigns(:opera)
+   end
+
   end
 
   test "should get opera from tag" do
--- test/integration/website_visitor_change_locale.rb
+++ test/integration/website_visitor_change_locale.rb
@@ -8,10 +8,19 @@ class WebsiteVisitorChangeLocale < ActionDispatch::IntegrationTest
   end
 
   test "user can view different page with different langhuage" do
-    get page_path(pages(:page_one), locale: 'it')
-    assert_response :success
     get page_path(pages(:page_one), locale: 'en')
+    assert_response :success
+    get page_path(pages(:page_one), locale: 'it')
     assert_response :redirect
+  end
+
+  test "user can see a opera page in different language" do
+    ['it', 'en'].each do |lng|
+      get page_path(pages(:page_with_opera), locale: lng)
+      assert_response :success
+      get opera_path(operas(:opera_one), locale: lng)
+      assert_response :success
+    end
   end
 
 end
